aeef5b2562b9e5f5829a5c7b4a6a46f13bd85ebb
larsb-dev <github@worldagainst.me>
2026-03-18 14:43:20 +0100

    add a post endpoint to add todo

--- api/function_app.py
+++ api/function_app.py
@@ -19,3 +19,20 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         status_code=200,
         mimetype="application/json"
     )
+
+@app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
+def add_todo(req: func.HttpRequest) -> func.HttpResponse:
+  try:
+    req_body = req.get_json()
+    new_todo = {
+      "id": max(todo["id"] for todo in todos) + 1,
+      "task": req_body.get("task")
+    }
+    todos.append(new_todo)
+    return func.HttpResponse(
+      json.dumps(new_todo),
+      status_code=201,
+      mimetype="application/json"
+    )
+  except ValueError:
+    return func.HttpResponse("Invalid request body", status_code=400)
